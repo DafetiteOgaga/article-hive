dd909fca1801bd637bcf8bc32d9a03a27e5cf30c
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-06-28 22:07:36 +0100

    the article hive - frontend component

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### article-hive and README.md were auto created using createRepo command in [*Custom Commands*](https://github.com/DafetiteOgaga/custom_commands)
+### The-Article-Hive and README.md were auto created using createRepo command in [*Custom Commands*](https://github.com/DafetiteOgaga/custom_commands)
 		
 
 
---
+++ static/hero.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+	const images = [
+		'./images/Adult-Book-Club.png',
+		'./images/GMA_Main_BookClubMontage_March_v01_ks_1710354921581_hpMain_16x9_1600.jpg',
+		'./images/Group-of-open-books-photographed-from-top-down.png',
+		'./images/how-to-host-a-virtual-book-club2-1585775401.jpg',
+		'./images/shutterstock_240069319.png'
+	];
+
+	let currentIndex = 0;
+	const heroElement = document.getElementsByClassName('hero')[0];
+
+	function changeBackgroundImage() {
+		heroElement.style.background = `url(${images[currentIndex]}) no-repeat center center`;
+		heroElement.style.backgroundSize = 'cover';
+		currentIndex = (currentIndex + 1) % images.length;
+	}
+
+	setInterval(changeBackgroundImage, 4500);
+
+	changeBackgroundImage();
+});
